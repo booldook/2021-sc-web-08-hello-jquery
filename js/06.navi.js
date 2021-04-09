53a6603189a9fe7046eb5dee902239147f153aac
booldook <booldook@gmail.com>
2021-04-09 16:01:12 +0900

    ...

--- js/06.navi.js
+++ js/06.navi.js
@@ -7,11 +7,20 @@ $('.sub-wrap').slideUp(0)
 
 
 /******************** 이벤트 등록 *********************/
-$('.navi > span').on('click', onNaviClick)
+// $('.navi > span').on('click', onNaviClick)
+$('.navi').on('mouseenter', onNaviEnter)
+$('.navi').on('mouseleave', onNaviLeave)
 
 
 /******************** 이벤트 콜백 *********************/
-function onNaviClick() {
+/* function onNaviClick() {
 	$('.sub-wrap').stop().slideUp(10)
 	$(this).next().stop().slideToggle(500)
+} */
+function onNaviEnter() {
+	$('.sub-wrap').stop().slideUp(0)
+	$(this).find('.sub-wrap').stop().slideDown(300)
+}
+function onNaviLeave() {
+	$('.sub-wrap').stop().slideUp(300)
 }
