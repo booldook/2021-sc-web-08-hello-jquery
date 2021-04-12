fa6030a896df63fa4bcea07f24eaf223da1a378e
booldook <booldook@gmail.com>
2021-04-12 12:16:42 +0900

    푸시!

--- js/07.popup.js
+++ js/07.popup.js
@@ -1,5 +1,5 @@
 /***************** 글로벌 설정 *****************/
-
+$('.modal-wrapper').hide()
 
 /***************** 사용자 함수 *****************/
 
@@ -9,6 +9,10 @@ $('.bt-link').on('click', onLinkClick)
 $('.bt-win').on('click', onWinClick)
 $('.bt-popup').on('click', onPopupClick)
 $('.bt-modal').on('click', onModalClick)
+
+$('.modal-wrapper').on('click', onModalWrapperClick)
+$('.modal-wrapper .modal-wrap').on('click', onModalWrapClick)
+$('.modal-wrapper .bt-close, .modal-wrapper .bt-close2').on('click', onModalCloseClick)
 
 
 /***************** 이벤트 콜백 *****************/
@@ -26,5 +30,17 @@ function onPopupClick() {
 }
 
 function onModalClick() {
-	
+	$('.modal-wrapper').show()
+}
+
+function onModalWrapperClick() {
+	$(this).hide()
+}
+
+function onModalWrapClick(e) {
+	e.stopPropagation()
+}
+
+function onModalCloseClick() {
+	$('.modal-wrapper').hide()
 }
