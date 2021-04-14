78efa2872fb89650aa317103b8e5525c08653c1e
booldook <booldook@gmail.com>
2021-04-14 14:38:30 +0900

    11.data init

--- js/11.data.js
+++ js/11.data.js
@@ -1,11 +1,26 @@
 /*************** 글로벌 설정 *****************/
-
+var prds = [
+	{ id: 1, src: '../img/cherries.jpg', title: '맛있는 체리', desc: '체리체리' },
+	{ id: 2, src: '../img/croissant.jpg', title: '맛있는 빵', desc: '빵빵빵' },
+	{ id: 3, src: '../img/popsicle.jpg', title: '맛있는 크림', desc: '크림크림' },
+	{ id: 4, src: '../img/salmon.jpg', title: '맛있는 연어', desc: '연어연어' },
+	{ id: 5, src: '../img/sandwich.jpg', title: '맛있는 샌드위치', desc: '샌드위치' },
+	{ id: 6, src: '../img/steak.jpg', title: '맛있는 스테이크', desc: '스테이크' },
+	{ id: 7, src: '../img/wine.jpg', title: '맛있는 와인', desc: '와인와인' }
+]
 
 /*************** 사용자 함수 *****************/
 
 
 /*************** 이벤트 등록 *****************/
-
+$('.bt-create').click(onCreate)
+$('.bt-remove').click(onRemove)
 
 /*************** 이벤트 콜백 *****************/
+function onCreate() {
 
+}
+
+function onRemove() {
+
+}
